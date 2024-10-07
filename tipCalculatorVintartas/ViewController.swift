6213ffe246ae66ba4f4d8d3acaba201dc0099e88
NV <nv2009123@gmail.com>
2024-10-07 18:49:31 -0500

    Initial Commit

---
+++ tipCalculatorVintartas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipCalculatorVintartas
+//
+//  Created by Nicholas Vintartas on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tipCalculatorVintartasTests/tipCalculatorVintartasTests.swift
@@ -0,0 +1,17 @@
+//
+//  tipCalculatorVintartasTests.swift
+//  tipCalculatorVintartasTests
+//
+//  Created by Nicholas Vintartas on 10/7/24.
+//
+
+import Testing
+@testable import tipCalculatorVintartas
+
+struct tipCalculatorVintartasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
