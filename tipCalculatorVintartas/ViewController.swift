7c6f8c0b9d4fe17968a608d585d02cf22639c9e6
NV <nv2009123@gmail.com>
2024-10-07 19:22:10 -0500

    Initial Commit

--- tipCalculatorVintartas/ViewController.swift
+++ tipCalculatorVintartas/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var percField: UITextField!
+    
+    @IBOutlet weak var peopleField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var splitLabel: UILabel!
+    
+    @IBOutlet weak var changeLabel: UILabel!
+    
+    var bill = 0.00
+    
+    var tip = 0.00
+    
+    var total = 0.00
+    
+    var people = 0
+    
+    var div = 0.00
+    
+    var change = 0.00
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        bill = Double(billField.text!)!
+        tip = Double(percField.text!)!*bill/100
+        people = Int(peopleField.text!)!
+        tip = round(100.0 * tip) / 100.0
+        total = bill + tip
+        div = round(100.0 * (total / Double(people))) / 100.0
+        while div * Double(people) < total {
+            div = div + 0.01
+        }
+        change = div*Double(people) - total
+        change = round(change * 100.0) / 100.0
+        tipLabel.text = "Tip: $\(tip)"
+        totalLabel.text = "Total: $\(total)"
+        splitLabel.text = "Each person should pay: $\(div)"
+        changeLabel.text = "Change due: $\(change)"
+    }
+    
 }
 
